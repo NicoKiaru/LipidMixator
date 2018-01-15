37de4be39b62c0352bf26af1fc686ce91ac7d4be
NicoKiaru <nicolas.chiaruttini@unige.ch>
2018-01-15 14:23:19 +0100

    Updates lipids echelon
    Adds fluorescent pi(3)p

--- lipids/lipids_echelon.js
+++ lipids/lipids_echelon.js
@@ -2,5 +2,5 @@ var lipids_echelon=[
 {value:"Biotin Phosphatidylinositol 3-phosphate | C-03B6",data:{source:"ECHELON", id:"C-03B6|4", mw:564}},
 {value:"GloPIPs BODIPY® TMR-PtdIns(4,5)P2, C16 | Red PIP2 | C-45M16a",data:{source:"ECHELON", id:"C-45M16a", mw:1528.5}},
 {value:"BODIPY®-FL-PI(4,5)P2, C16  | Green PIP2 | C-45F16a",data:{source:"ECHELON", id:"C-45F16a", mw:1422.4}},
-
+{value:"GloPIPs BODIPY® TMR PI(3)P  | Red PI(3)P | C-03M6a",data:{source:"ECHELON", id:"C-03M6a", mw:1207.1}},
 ];
